b92762fd120284f8db0823b2c9818700cd053f0d
Adam Krupa <adam.krupa@syzygy.pl>
2022-10-04 22:01:35 +0200

    fix: remove unnecessary things. changes for readme cross platforms support

--- src/utils/git.util.ts
+++ src/utils/git.util.ts
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
 
 export async function getAuthor(projectPath: string) {
-    return execSync(`git -C "./${projectPath}" config user.email`).toString()
+    return execSync(`git -C ${projectPath} config user.email`).toString()
 }
